a8ad5d675a43cd54a6a72473b155ec9d524e4a0d
iruz17 <iruzakun17@gmail.com>
2020-08-18 22:52:00 +0700

    ambil

--- client/plugins/http-common.js
+++ client/plugins/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: `${location.protocol}//${location.hostname}:${location.port}/`,
+  baseURL: `${location.protocol}//${location.hostname}:8080/`,
   headers: {
     'Content-type': 'application/json',
   },
